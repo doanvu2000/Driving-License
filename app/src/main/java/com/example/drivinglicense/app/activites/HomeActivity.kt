4b93f11cbc29bda793f90cf13c4cbeb5716cc290
doanvu2000 <dauphong13@gmail.com>
2022-04-13 00:49:02 +0700

    base layout home screen with slide and some item action

--- app/src/main/java/com/example/drivinglicense/app/activites/HomeActivity.kt
+++ app/src/main/java/com/example/drivinglicense/app/activites/HomeActivity.kt
@@ -7,38 +7,93 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.drivinglicense.R
+import com.example.drivinglicense.app.adapter.ActionAdapter
+import com.example.drivinglicense.app.entity.ItemAction
+import com.example.drivinglicense.component.widgets.recyclerview.RecyclerUtils
 import com.example.drivinglicense.databinding.ActivityMainBinding
 import com.example.drivinglicense.pref.showDevelopMessage
 import com.example.drivinglicense.pref.toastMessage
+import java.util.ArrayList
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val actionAdapter by lazy {
+        ActionAdapter()
+    }
+
+    private lateinit var listAction: MutableList<ItemAction>
+
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.loadingView.start()
         supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.green)))
-//        supportActionBar?.setDisplayShowTitleEnabled(true)
-//        supportActionBar?.setDisplayShowHomeEnabled(true)
-//        supportActionBar?.setIcon(R.drawable.ic_settings)
+        initView()
+        initData()
+        initListener()
+    }
+
+    private fun initView() {
+        initSlide()
+    }
+
+    private fun initSlide() {
+        val listSlide =
+            arrayListOf(
+                R.drawable.exam,
+                R.drawable.book,
+                R.drawable.star,
+                R.drawable.law,
+                R.drawable.stop
+            )
+        for (item in listSlide) {
+            val imageView = ImageView(this)
+            imageView.setImageResource(item)
+            binding.slide.addView(imageView)
+        }
+        binding.slide.flipInterval = 1500
+        binding.slide.isAutoStart = true
+        val animationSlideIn = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left)
+        val animationSlideOut = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right)
+        binding.slide.inAnimation = animationSlideIn
+        binding.slide.outAnimation = animationSlideOut
+    }
+
+    private fun initData() {
+        RecyclerUtils.setGridManager(this, binding.rcvItem, 3, actionAdapter)
+        listAction = ArrayList()
+        val item1 = ItemAction(getString(R.string.text_exam), R.drawable.exam)
+        val item2 = ItemAction(getString(R.string.text_learning_theory), R.drawable.book)
+        val item3 = ItemAction(getString(R.string.text_road_signs), R.drawable.stop)
+        val item4 = ItemAction(getString(R.string.text_tips), R.drawable.star)
+        val item5 = ItemAction(getString(R.string.text_search_law), R.drawable.law)
+        listAction = arrayListOf(item1, item2, item3, item4, item5)
+        actionAdapter.addData(listAction)
+    }
+
+    private fun initListener() {
+        actionAdapter.onCLickItem = {
+            toastMessage(this, listAction[it].title)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_setting,menu)
+        menuInflater.inflate(R.menu.menu_setting, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.item_license_A1 ->{
-                toastMessage(this,getString(R.string.text_chose_license_A1))
+        when (item.itemId) {
+            R.id.item_license_A1 -> {
+                toastMessage(this, getString(R.string.text_chose_license_A1))
             }
-            R.id.item_license_A2 ->{
-                toastMessage(this,getString(R.string.text_chose_license_A2))
+            R.id.item_license_A2 -> {
+                toastMessage(this, getString(R.string.text_chose_license_A2))
             }
         }
         return false
