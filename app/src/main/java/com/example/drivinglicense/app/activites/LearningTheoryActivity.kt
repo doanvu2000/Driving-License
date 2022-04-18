5a786e398d4509315b7aba37b720ecc00cac94be
doanvu2000 <dauphong13@gmail.com>
2022-04-18 23:01:29 +0700

    create ToolBarApp and add to activity

--- app/src/main/java/com/example/drivinglicense/app/activites/LearningTheoryActivity.kt
+++ app/src/main/java/com/example/drivinglicense/app/activites/LearningTheoryActivity.kt
@@ -1,14 +1,17 @@
 package com.example.drivinglicense.app.activites
 
+import com.example.drivinglicense.R
 import com.example.drivinglicense.component.activity.BaseCoreActivity
 import com.example.drivinglicense.databinding.ActivityLearningTheoryBinding
 
 class LearningTheoryActivity : BaseCoreActivity<ActivityLearningTheoryBinding>() {
     override fun initView() {
         supportActionBar?.hide()
+        binding.toolbar.setTitle(getString(R.string.text_learning_theory))
     }
 
     override fun initListener() {
+        binding.toolbar.onLeftClickListener = { onBackPressed() }
     }
 
     override fun initData() {
