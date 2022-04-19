01e23d6ec7f992146c4dc35a6940a0c39ac7fce8
doanvu2000 <dauphong13@gmail.com>
2022-04-20 00:47:14 +0700

    fill ddemo data in string

--- app/src/main/java/com/example/drivinglicense/app/entity/Question.kt
+++ app/src/main/java/com/example/drivinglicense/app/entity/Question.kt
@@ -2,8 +2,8 @@ package com.example.drivinglicense.app.entity
 
 data class Question(
     val questionId: Int,
-    val examId: Int,
+    val examId: Int?,
     val content: String,
-    val image: String,
+    val image: String?,
     val isImportant: Boolean //câu điểm liệt (sai => trượt)
 )
